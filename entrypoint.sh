72d05a22073c272edc7c767b5d6666bcf249a09d
Juiomn <104608232+Juiomn@users.noreply.github.com>
2023-09-11 09:24:21 +0000

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
 # 定义 UUID 及伪装路径、哪吒面板参数，请自行修改. (注意:伪装路径以 / 符号开始,为避免不必要的麻烦,请不要使用特殊符号.)
-UUID='7a558421-14ee-40f1-a738-b5c587b9bbfe'
-VMESS_WSPATH='/7a558421-14ee-40f1-a738-b5c587b9bbfe-vmess'
-VLESS_WSPATH='/7a558421-14ee-40f1-a738-b5c587b9bbfe-vless'
+UUID='62498d25-380e-4dbf-9e7d-7bc30bf99131'
+VMESS_WSPATH='/62498d25-380e-4dbf-9e7d-7bc30bf99131-vmess'
+VLESS_WSPATH='/62498d25-380e-4dbf-9e7d-7bc30bf99131-vless'
 TROJAN_WSPATH='/trojan'
 SS_WSPATH='/shadowsocks'
 NEZHA_SERVER=''
